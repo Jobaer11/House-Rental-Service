174e36ca7747caa29998d4ec4b8351c4afcd04f0
Jobaer11 <jobaer11-email@example.com>
2026-02-22 09:28:39 +0600

    feat: add Firebase initialization configuration

---
+++ client/src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCpj7jPiRSTA-eRHSTO9KVFIP87etaFl8o",
+  authDomain: "mern-state-f7a68.firebaseapp.com",
+  projectId: "mern-state-f7a68",
+  storageBucket: "mern-state-f7a68.appspot.com",
+  messagingSenderId: "197593343621",
+  appId: "1:197593343621:web:095385a103203ed18d2bec",
+  measurementId: "G-V8GLCS55EM"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
